fa229c0a7ce26428d4dbebc120f65eb537b9ab26
Libia <Baka65311131@gmail.com>
2025-05-15 19:53:45 -0400

    Segundo cmmit con ajustes

--- usecases/build.gradle.kts
+++ usecases/build.gradle.kts
@@ -11,3 +11,8 @@ kotlin {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
     }
 }
+dependencies {
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(project(":domain"))
+    implementation(project(":data"))
+}
